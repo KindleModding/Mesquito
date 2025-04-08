e573eab280f56a23563dc81fac944d7339621703
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-04-08 10:41:57 +0100

    Add secondaryStore file (noted by jacobt)

--- mesquito/main.js
+++ mesquito/main.js
@@ -5,7 +5,7 @@
 //===============================//
 
 var versionInt = 2;
-var versionString = 'v0.3.0'
+var versionString = 'v0.3.1'
 
 // Clear localStorage log
 //window.localStorage.setItem("latest.log", JSON.stringify([])) // Empty log
